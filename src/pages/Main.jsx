323e92d23a48ffb43cce2a4fee6e1f582a3403a8
Erhan <erhantezer68@gmail.com>
2023-01-23 18:03:41 +0300

    updated

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
 import { AuthContext } from "../context/AuthContextProvider";
 
 
@@ -8,66 +9,68 @@ const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
-const [movies, setMovies] = useState([]);
-const [search, setSearch] = useState("");
-const [loading, setLoading] = useState(false);
-const {currentUser} = useContext(AuthContext)
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { currentUser } = useContext(AuthContext)
 
-const getMovies = (API) => {
-  setLoading(true);
-  axios.get(API)
-  .then((res) => setMovies(res.data.results))
-  .catch((err) => console.log(err))
-  .finally(() => setLoading(false))
-}
-
-useEffect(() => {
-  getMovies(FEATURED_API)
-}, []);
-
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  if (search && currentUser) {
-    getMovies(SEARCH_API + search)
-    search("")
-  } else if (!currentUser) {
-    alert("please log in to see details")
-  } else {
-    alert("please enter a text")
+  const getMovies = (API) => {
+    setLoading(true);
+    axios.get(API)
+      .then((res) => setMovies(res.data.results))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false))
   }
-}
+
+
+  useEffect(() => {
+    getMovies(FEATURED_API)
+  }, []);
+
+console.log(movies)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (search && currentUser) {
+      getMovies(SEARCH_API + search)
+      search("")
+    } else if (!currentUser) {
+      alert("please log in to see details")
+    } else {
+      alert("please enter a text")
+    }
+  }
 
   return (
     <>
-    <form 
-    className="flex justify-center p-2" 
-    onSubmit={handleSubmit}
-    >
-      <input
-        type="search"
-        className="w-80 h-8 rounded-md outline-none border p-1 m-2"
-        placeholder="Search a movie..."
-        onChange={(e) => setSearch(e.target.value)}
-        value={search}
-      />
-      <button className="dark:text-white" type="submit">
-        Search
-      </button>
-    </form>
-    <div className="flex justify-center flex-wrap">
-      
-        <div
-          className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
-          role="status"
-        >
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        
-       
-      
-    </div>
-  </>
+      <form
+        className="flex justify-center p-2"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="search"
+          className="w-80 h-8 rounded-md outline-none border p-1 m-2"
+          placeholder="Search a movie..."
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button className="dark:text-inherit" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="flex justify-center flex-wrap">
+        {loading ? (
+          <div
+            className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
+            role="status"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        ) : (
+        movies.map((movie) =><MovieCard key={movie.id} {...movie}/>)
+        )}
+      </div>
+    </>
   )
 }
 
