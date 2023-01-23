125d2de2ede5069feaf4bc98c753443ffa21662e
Erhan <erhantezer68@gmail.com>
2023-01-23 18:07:57 +0300

    updated card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContextProvider";
 
-const MovieCard = () => {
+
+
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const defaultImage =
+  "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
+
+const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
+  const navigate = useNavigate();
+  const {currentUser} = useContext(AuthContext);
+  
   return (
     <div>MovieCard</div>
   )
